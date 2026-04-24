a72f6c4f5759a7ba12160a40def0584c81a38b74
hammad sunsara <hammadsunsara9@gmail.com>
2026-04-24 19:12:57 +0530

    changes

--- src/services/employee.Service.ts
+++ src/services/employee.Service.ts
@@ -1,5 +1,4 @@
-// const BASE_URL = import.meta.env.VITE_BASE_URL;
-const BASE_URL = "http://localhost:5000";
+ const BASE_URL = import.meta.env.VITE_BASE_URL;
 const API_URL = `${BASE_URL}/api/employees`;
 
 const getToken = () => localStorage.getItem("token");
