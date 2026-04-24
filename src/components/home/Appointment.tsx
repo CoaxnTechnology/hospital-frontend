58c479be5edbec805908a2c6ce8ed6125aa251f8
hammad sunsara <hammadsunsara9@gmail.com>
2026-04-24 11:59:09 +0530

    new logic add for slot morning+evening show

--- src/components/home/Appointment.tsx
+++ src/components/home/Appointment.tsx
@@ -1,5 +1,5 @@
-// const BASE_URL = import.meta.env.VITE_BASE_URL;
-const BASE_URL = "http://localhost:5000";
+ const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 import { useEffect, useState, useRef } from "react";
 import { getDoctors } from "../../services/doctor.service";
 import { useLocation, useNavigate } from "react-router-dom";
